f530ac86ddfef0ef0876b7b488ba64d90ed2c98c
ritez_0x53 <110099293+ritez-0x53@users.noreply.github.com>
2024-11-10 15:52:54 +0530

    Update showModel.js

--- model/showModel.js
+++ model/showModel.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
 const { model, Schema } = mongoose;
 
+// Schema for the 'shows' collection with movie name, time slot, and seat availability
 const showSchema = Schema({
-  movieName: {
-    type: String,
-    required: true,
-  },
-  timeSlot: {
-    type: String,
-    required: true,
-  },
+  movieName: { type: String, required: true },
+  timeSlot: { type: String, required: true },
   A1: Number,
   A2: Number,
   A3: Number,
@@ -18,5 +13,8 @@ const showSchema = Schema({
   D2: Number,
 });
 
+// Model for interacting with 'shows' collection in MongoDB
 const Shows = model("shows", showSchema);
+
+// Export the Shows model
 module.exports = { Shows };
